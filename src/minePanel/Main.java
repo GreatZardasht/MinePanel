421b3569ce6017a6964c128660b5b0544804a354
Will Eccles <willeccles860+github@gmail.com>
2015-03-02 17:42:20 -0500

    Rework ServerRunner
    This fixes the process that runs the server. Works fine, but with no
    input to the console. Also, still no GUI. Must run the Jar from cmd
    (java -jar [name].jar).

--- src/minePanel/Main.java
+++ src/minePanel/Main.java
@@ -3,13 +3,18 @@ package minePanel;
 public class Main {
 	//static Panel mainGUI = new Panel();
 	static FileManager fm = new FileManager();
+	/**
+	 * This is my personal JRE location. Should NEVER be used in production.
+	 */
+	final static String MY_JAVA_LOC = "c:\\Program Files\\Java\\jre1.8.0_31\\bin\\java.exe";
 	
 	public static void main(String[] args) {
 		// Enable the GUI (a bit necessary)
 		//mainGUI.setEnabled(true);
 		//mainGUI.setVisible(true);
 		
-		
-		FileManager.setProp("motd", "penis");
+		// Doing this in tests with a server jar named "server.jar" worked. No input to console yet, but that's next on the list.
+		ServerRunner runner = new ServerRunner("server.jar", 3096, MY_JAVA_LOC, true, null);
+		runner.startServer();
 	}
 }
--- src/minePanel/ServerRunner.java
+++ src/minePanel/ServerRunner.java
@@ -4,46 +4,63 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-// A lot of this class is thanks to the answer here: http://stackoverflow.com/questions/15700879/how-to-run-a-java-executable-jar-in-another-java-program
+import javax.swing.JTextPane;
 
+// A lot of this class is thanks to the answer here: http://stackoverflow.com/questions/27684050/execute-jar-and-read-the-console-output-using-inputstream-java
+
+// EDIT: Basically completely renovating this whole class.
+
+/**
+ * @author Will Eccles
+ */
 public class ServerRunner {
+	private String jarName;
+	private int usedRAM;
+	private String javaLocation;
+	private boolean force64;
+	@SuppressWarnings("unused")
+	private JTextPane jtp;
 	
-	public ServerRunner(String jarName, String RAM, String javaLocation) {
-		
-		ProcessBuilder pb = new ProcessBuilder("java", "-jar", jarName);
-		
+	/**
+	 * @param jarName The name of the server jar.
+	 * @param RAM The amount of RAM in MB to use.
+	 * @param javaLoc Location of java.
+	 * @param force64bit Whether or not to force 64-bit running of the server.
+	 */
+	public ServerRunner(String serverJarName, int RAM, String javaLoc, boolean force64bit, JTextPane pane) {
+		jarName = serverJarName;
+		usedRAM = RAM;
+		javaLocation = javaLoc;
+		force64 = force64bit;
+		jtp = pane;
+	}
+	
+	/**
+	 * Run the server.
+	 */
+	public void startServer() {
+		// Run the server based on the arguments supplied to the instance of the class
 		try {
+			ProcessBuilder pb = new ProcessBuilder(javaLocation, "-jar", jarName, ("-Xmx"+usedRAM+"M"), ("Xms"+usedRAM+"M"), "nogui", force64?"d64":"");
+			pb.redirectErrorStream(true);
 			Process p = pb.start();
-			LogStreamReader lsr = new LogStreamReader(p.getInputStream());
-			Thread thread = new Thread(lsr, "LogStreamReader");
-			thread.start();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
-
-class LogStreamReader implements Runnable {
-	
-	private BufferedReader reader;
-	
-	public LogStreamReader(InputStream is) {
-		this.reader = new BufferedReader(new InputStreamReader(is));
-	}
-	
-	public void run() {
-		try {
-			String line = reader.readLine();
-			while (line != null) {
+		
+			InputStream in = p.getInputStream();
+			InputStreamReader isr = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(isr);
+			
+			String line;
+			
+			while ((line = br.readLine()) != null) {
 				System.out.println(line);
-				line = reader.readLine();
 			}
-			reader.close();
-		}
-		catch (Exception e) {
+			
+			p.waitFor();
+			p.destroy();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 }
